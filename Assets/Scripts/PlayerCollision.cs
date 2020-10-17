bd2fb62aa9ddacbf882ae26f45ffab3b6fe4e88f
solidogen <solidogen@gmail.com>
2020-10-17 13:02:11 +0200

    Added player collision and obstacles

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerMovement movement = default;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("Hit an obstacle");
+            movement.enabled = false;
+        }
+    }
+}
