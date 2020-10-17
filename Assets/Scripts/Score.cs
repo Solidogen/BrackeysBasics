99dee8b55575b821da691c5fcfb5d10017d89815
solidogen <solidogen@gmail.com>
2020-10-17 13:40:04 +0200

    Added font and score text

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField]
+    private Transform playerTransform = default;
+
+    [SerializeField]
+    private Text text = default;
+
+    void Update()
+    {
+        text.text = playerTransform.position.z.ToString("0");
+    }
+}
