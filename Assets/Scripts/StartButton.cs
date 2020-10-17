5bef38524dc812f66045c94851b530e697fbeedf
solidogen <solidogen@gmail.com>
2020-10-17 23:54:55 +0200

    Start scene

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    [SerializeField]
+    private Button button = default;
+
+    void Awake()
+    {
+        button.onClick.AddListener(delegate {
+            GoToNextLevel();
+        });
+    }
+
+    private void GoToNextLevel()
+    {
+        FindObjectOfType<GameManagement>().GoToNextLevel();
+    }
+}
