1971213a6e4ae72ac3b6526b865b1be3de25e4a8
solidogen <solidogen@gmail.com>
2020-10-18 15:36:44 +0200

    Initialize game in fullscreen

---
+++ Assets/Scripts/InitializeGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializeGame : MonoBehaviour
+{
+    void Awake()
+    {
+        Screen.fullScreen = true;
+    }
+}
